9659033042df3997b47710e279b91d68c2590940
Liz N <LNga@users.noreply.github.com>
2018-05-01 16:52:41 +0200

    Update deploy.js

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -3,8 +3,8 @@ const Web3 = require('web3');
 const compiledFactory = require('./build/cocoContractFactory.json');
 
 const provider = new HDWalletProvider(
-  'usage saddle chunk detect nephew predict essay caution turkey hope spare define',
-  'https://rinkeby.infura.io/ip4HwWHBjZmslcVOaWzx'
+  '***** **** **** ****** ******* ******* ********** ********* *****', //seeds
+  'https://rinkeby.infura.io/***'
 );
 const web3 = new Web3(provider);
 
@@ -22,29 +22,3 @@ const deploy = async () => {
   console.log('Contract deployed to', result.options.address);
 };
 deploy();
-
-/*
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
-
-const provider = new HDWalletProvider(
-  'usage saddle chunk detect nephew predict essay caution turkey hope spare define',
-  'https://rinkeby.infura.io/ip4HwWHBjZmslcVOaWzx'
-);
-const web3 = new Web3(provider);
-
-const deploy = async () => {
-  const accounts = await web3.eth.getAccounts();
-
-  console.log('Attempting to deploy from account', accounts[0]);
-
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
-    .send({ gas: '1000000', from: accounts[0] });
-
-  console.log(interface);
-  console.log('Contract deployed to', result.options.address);
-};
-deploy();
-*/
